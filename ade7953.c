4651415e40d87ee44cbafab4b25fc48081931060
André Maree <ammaree@users.noreply.github.com>
2023-08-20 10:08:48 +0200

    Update ade7953.c

--- ade7953.c
+++ ade7953.c
@@ -15,6 +15,7 @@
 
 #if (halHAS_ADE7953 > 0)
 #include "ade7953.h"
+#include "hal_i2cm.h"
 #include "hal_storage.h"
 #include "FreeRTOS_Support.h"
 #include "options.h"
@@ -42,61 +43,87 @@
 
 // ########################################### Macros ##############################################
 
-//	#if (ade7953USE_I2C == 1)
-//	#elif (ade7953USE_SPI == 1)
-//	#endif
+
+// ######################################### Structures ############################################
+
 
 // ####################################### Private variables #######################################
 
+static const i32_t NVSDefaults[ade7953NUM_CHAN * 6] = {
+	0, 0, 0, 0, 0, 0,
+	#if	(ade7953USE_CH2 > 0)		// Line 2 / Neutral
+	0, 0, 0, 0, 0, 0,
+	#endif
+};
+
+const u16_t CfgRegs[ade7953NUM_CHAN * 6] = {
+	regAIGAIN, regAVGAIN, regAWGAIN, regAVARGAIN, regAVAGAIN, regPHCALA,
+	#if	(ade7953USE_CH2 > 0)		// Line 2 / Neutral
+	regBIGAIN, regBVGAIN, regBWGAIN, regBVARGAIN, regBVAGAIN, regPHCALB,
+	#endif
+};
 
 // ####################################### Public variables ########################################
 
 u8_t NumADE7953 = 0;
-ade7953_t sADE7953[halHAS_ADE7953] = { 0 };
-ade7953nvs_t ade7953nvs[halHAS_ADE7953] = { 0 };
-const ade7953nvs_t ade7953Def = { 0 };
+ade7953_t ade7953[halHAS_ADE7953] = { 0 };
 
 // ############################### common support routines #########################################
 
-int ade7953CalcRegSize(u16_t Reg) {
-	if (Reg == ade7953REG_VERSION || Reg == ade7953REG_EX_REF || Reg < 0x100)
-		return 1;
-	if (Reg < 0x200)
-		return 2;
-	if (Reg < 0x300)
-		return 3;
+static int ade7953CalcRegSize(u16_t Reg) {
+	if (Reg < 0x100 || Reg == regVERSION || Reg == regEX_REF) return 1;
+	if (Reg < 0x200) return 2;
+	if (Reg < 0x300) return 3;
 	return 4;
 }
 
 int ade7953Write(ade7953_t * psADE7953, u16_t Reg, i32_t Val) {
 	IF_myASSERT(debugPARAM, halCONFIG_inSRAM(psADE7953));
-	int iRV, Size = ade7953CalcRegSize(Reg);
-	#if (ade7953USE_I2C == 1)
-	iRV = ade7953WriteI2C(psADE7953, Reg, Size, Val);
-	#elif (ade7953USE_SPI == 1)
-	iRV = ade7953WriteSPI(psADE7953, Reg, Size, Val);
-	#endif
-	if ((iRV < erSUCCESS) && !(Reg == ade7953REG_CONFIG && (Val & ade7953REG_CONFIG_SWRST)))
+	int Size = ade7953CalcRegSize(Reg);
+	u8_t caBuf[6];
+	caBuf[0] = (Reg >> 8) & 0xFF;
+	caBuf[1] = Reg & 0xFF;
+	int Len = 2;
+	while (Size--) caBuf[Len++] = (Val >> (8 * Size)) & 0xFF;		// correct LE -> LE conversion
+
+	IF_EXEC_1(debugTIMING, xSysTimerStart, stADE7953W);
+	int iRV = halI2CM_Queue(psADE7953->psI2C, i2cW_B, caBuf, Len, NULL, 0, (i2cq_p1_t)NULL, (i2cq_p2_t)NULL);
+	IF_EXEC_1(debugTIMING, xSysTimerStop, stADE7953W);
+
+	if ((iRV < erSUCCESS) && !(Reg == regCONFIG && (Val & regCONFIG_SWRST)))
 		SL_ERR("Error Reg=0x%X iRV=%d", Reg, iRV);
 	return iRV;
 }
 
 int ade7953Read(ade7953_t * psADE7953, u16_t Reg, void * pVal) {
 	IF_myASSERT(debugPARAM, halCONFIG_inSRAM(psADE7953));
-	int iRV, Size = ade7953CalcRegSize(Reg);
-	#if (ade7953USE_I2C == 1)
-	iRV = ade7953ReadI2C(psADE7953, Reg, Size, pVal);
-	#elif (ade7953USE_SPI == 1)
-	iRV = ade7953ReadSPI(psADE7953, Reg, Size, pVal);
-	#endif
+	u8_t caBuf[2] = { (Reg >> 8) & 0xFF,  Reg & 0xFF };
+	int Size = ade7953CalcRegSize(Reg);
+	//
+	IF_EXEC_1(debugTIMING, xSysTimerStart, stADE7953R);
+	int iRV = halI2CM_Queue(psADE7953->psI2C, psADE7953->cb ? i2cWRC_F : i2cWR_B, caBuf,
+		sizeof(caBuf),pVal, Size, (i2cq_p1_t)psADE7953->cb, (i2cq_p2_t) (void *)psADE7953);
+	IF_EXEC_1(debugTIMING, xSysTimerStop, stADE7953R);
+	//
+	if (Size > 1) {
+		u8_t * pU8 = pVal;
+		u8_t U8 = pU8[0];
+		pU8[0] = pU8[Size-1];
+		pU8[Size-1] = U8;
+	}
 	return iRV;
+}
+
+u16_t ade7953ReadConfig(ade7953_t * psADE7953) {
+	ade7953Read(psADE7953, regCONFIG, &psADE7953->oth.cfg.val);
+	return psADE7953->oth.cfg.val;
 }
 
 /*
  * @brief	Callback handler after reading both IRQSTATA & IRQSTATB
  */
 void IRAM_ATTR ade7953IRQ_CB(void * Arg) {
-	ade7953_t * psADE7953 = (ade7953_t *) Arg;
+	CPTL(); ade7953_t * psADE7953 = (ade7953_t *) Arg;
 	ade7953ReportStatus(NULL, psADE7953);
 }
 
@@ -106,72 +133,73 @@ void IRAM_ATTR ade7953IRQ_CB(void * Arg) {
  * @return
  **/
 void IRAM_ATTR ade7953IntHandler(void * Arg) {
-	u8_t eDev = (int) Arg;
-	IF_myASSERT(debugPARAM, eDev < halHAS_ADE7953);
-	ade7953_t * psADE7953 = &sADE7953[eDev];
+	CPTL(); u8_t eDev = (int) Arg;
+	IF_myASSERT(debugPARAM, eDev < halHAS_ADE7953); // @suppress("Type cannot be resolved")
+	ade7953_t * psADE7953 = &ade7953[eDev];
+
 	// schedule 1st read, just to update in SRAM
-	ade7953Read(psADE7953, ade7953REG_IRQSTATA, &psADE7953->sIRQSTATA);	// read RSTIRQSTATA to reset?
+	psADE7953->cb = NULL;
+	ade7953Read(psADE7953, regRSTIRQSTATA, &psADE7953->oth.is_a); // @suppress("Symbol is not resolved")
+
 	// schedule 2nd read with callback handler
-	psADE7953->CallBack = ade7953IRQ_CB;				// preload callback address
-	ade7953Read(psADE7953, ade7953REG_IRQSTATB, &psADE7953->sIRQSTATB);	// read RSTIRQSTATB to reset?
-	psADE7953->CallBack = NULL;							// reset callback address
+	psADE7953->cb = &ade7953IRQ_CB;
+	ade7953Read(psADE7953, regRSTIRQSTATB, &psADE7953->oth.is_b); // @suppress("Symbol is not resolved")
 }
 
-/*int	ade7953LoadNVSConfig(u8_t eChan, u8_t Idx) {
-	IF_myASSERT(debugPARAM, Idx < ade7953CALIB_NUM);
-	size_t	SizeBlob = ade7953CALIB_NUM * sizeof(ade7953nvs_t);
-	ade7953nvs_t * psCalib = pvRtosMalloc(SizeBlob);
-	int iRV = halSTORAGE_ReadBlob(halSTORAGE_STORE, halSTORAGE_KEY_ADE7953, psCalib, &SizeBlob, ESP_OK);
-	if (iRV == erSUCCESS) {
-		//	Add code
-	} else {
-		SL_ERR("Failed Ch=%d config=%d", eChan, Idx);
-	}
-	vRtosFree (psCalib);
-	return iRV;
-}*/
-
-/*u8_t ade7953CalcInfo(epw_t * psEW) {
-	xEpWorkToUri(psEW);
-	psEW->idx = psEW->uri - URI_ADE7953_E_ACT_FWD_0;
-	psEW->eChan = 0;
-	// In case of multiple channels, adjust till in range....
-	while (psEW->idx >= ADE7953_NUMURI_0) {
-		psEW->idx -= ADE7953_NUMURI_0;
-		psEW->eChan++;
-	}
-	IF_myASSERT(debugRESULT, (psEW->idx < ADE7953_NUMURI_0) && (psEW->eChan < halHAS_ADE7953));
-	return psEW->idx;
-}*/
-
-// ############################## identification & initialization ##################################
-
-int	ade7953Identify(void * pVoid) {
-	ade7953_t * psADE7953 = &sADE7953[NumADE7953];
-	psADE7953->pin = ade7953IRQ_PIN1;
+void ade7953InitIRQ(int DevIdx) {
 	gpio_config_t irq_pin_cfg = {
-		.pin_bit_mask = 1ULL << ade7953IRQ_PIN1,
+		.pin_bit_mask = 1ULL << ade7953PIN_IRQ,
 		.mode = GPIO_MODE_INPUT,
 		.pull_up_en = GPIO_PULLUP_DISABLE ,
 		.pull_down_en = GPIO_PULLDOWN_DISABLE,
 		.intr_type = GPIO_INTR_NEGEDGE,
 	};
 	ESP_ERROR_CHECK(gpio_config(&irq_pin_cfg));
-	halGPIO_IRQconfig(psADE7953->pin, ade7953IntHandler, psADE7953);
+	halGPIO_IRQconfig(ade7953PIN_IRQ, ade7953IntHandler, (void *) DevIdx);
+}
 
-#if (ade7953USE_I2C == 1)
-	psADE7953->psI2C = pVoid;
-#elif (ade7953USE_SPI == 1)
-	psADE7953->hndl = pVoid;
-#endif
-	int iRV;
-	u8_t Ver = 0;
-	iRV = ade7953Read(psADE7953, ade7953REG_VERSION, &Ver);
+int	ade7953LoadNVSConfig(u8_t eChan, u8_t Idx) {
+	IF_myASSERT(debugPARAM, eChan < halHAS_ADE7953 && Idx < ade7953CALIB_NUM);
+	size_t	Size = ade7953CALIB_NUM * sizeof(ade7953nvs_t);
+	ade7953nvs_t * psCal = pvRtosMalloc(Size);
+	int iRV = halSTORAGE_ReadBlob(halSTORAGE_STORE, ade7953STORAGE_KEY, psCal, &Size, ESP_ERR_NVS_NOT_FOUND);
+	if ((iRV != erSUCCESS) || (Size != (ade7953CALIB_NUM * sizeof(ade7953nvs_t)))) {
+		memset(psCal, 0, Size = ade7953CALIB_NUM * sizeof(ade7953nvs_t));
+		memcpy(psCal, &NVSDefaults, NO_MEM(NVSDefaults));
+		iRV = halSTORAGE_WriteBlob(halSTORAGE_STORE, ade7953STORAGE_KEY, psCal, Size);
+		IF_myASSERT(debugRESULT, iRV == erSUCCESS);
+		if (Idx) {
+			SL_WARN("NVS config (%d) not found, default created/loaded", Idx);
+			Idx = 0;
+		}
+	}
+	ade7953_t * psADE7953 = &ade7953[eChan];
+	for( int i = 0; i < NO_MEM(CfgRegs); ++i) ade7953Write(psADE7953, CfgRegs[i], psCal->val[i]);
+	vRtosFree (psCal);
+	return iRV;
+}
+
+// ############################## identification & initialization ##################################
+
+int	ade7953Identify(i2c_di_t * psI2C) {
+	psI2C->TRXmS = 10;									// default device timeout
+	psI2C->CLKuS = 400;									// Max 13000 (13mS)
+	psI2C->Test = 1;									// test mode
+	psI2C->Type = i2cDEV_ADE7953;
+	psI2C->Speed = i2cSPEED_400;
+	psI2C->DevIdx = NumADE7953;
+	ade7953_t * psADE7953 = &ade7953[psI2C->DevIdx];
+	psADE7953->psI2C = psI2C;
+	//
+	int iRV = ade7953Read(psADE7953, regVERSION, &psADE7953->oth.ver);
+	psI2C->Test = 0;
 	if (iRV < erSUCCESS) {
 		SL_ERR("Device Failed!!!");
-	} else {
-		IF_SL_INFO(debugTRACK, "Silicon Version=0x%0hhX", Ver);
+		psADE7953->psI2C = NULL;
+		return erFAILURE;
 	}
+	SL_WARN("Silicon Version=0x%0hhX", psADE7953->oth.ver);
+	++NumADE7953;
 	return iRV;
 }
 
@@ -180,47 +208,35 @@ int	ade7953Identify(void * pVoid) {
  * @param
  * @return
  */
-int ade7953Config(void * pVoid) {
-	ade7953_t * psADE7953 = &sADE7953[NumADE7953];
-	/* Check that blob with CALibration and ADJustment values exists
-	 * If not existing, create with factory defaults as first record */
-	size_t	SizeBlob = ade7953CALIB_NUM * sizeof(ade7953nvs_t);
-	ade7953nvs_t * psCalib = pvRtosMalloc(SizeBlob);
-	int iRV = halSTORAGE_ReadBlob(halSTORAGE_STORE, halSTORAGE_KEY_ADE7953, psCalib, &SizeBlob, ESP_ERR_NVS_NOT_FOUND);
-	if ((iRV != erSUCCESS) || (SizeBlob != (ade7953CALIB_NUM * sizeof(ade7953nvs_t)))) {
-		memset(psCalib, 0, SizeBlob = ade7953CALIB_NUM * sizeof(ade7953nvs_t));
-		memcpy(psCalib, &ade7953Def, sizeof(ade7953nvs_t));
-		iRV = halSTORAGE_WriteBlob(halSTORAGE_STORE, halSTORAGE_KEY_ADE7953, psCalib, SizeBlob);
-		IF_myASSERT(debugRESULT, iRV == erSUCCESS);
-	} else {
-		memcpy(&psADE7953->sCurCalib, &psCalib[0], sizeof(ade7953nvs_t));
-	}
-	vRtosFree(psCalib);
-
-	iRV = ade7953Write(psADE7953, ade7953REG_CONFIG, ade7953REG_CONFIG_SWRST);
-    do {
-		vTaskDelay(pdMS_TO_TICKS(10));
-    } while (!ade7953Read(psADE7953, ade7953REG_IRQSTATA, &psADE7953->sIRQSTATA) || !(psADE7953->sIRQSTATA.val & ade7953REG_IRQSTATA_RESET));
-
-    ade7953Write(psADE7953, ade7953REG_CONFIG, 0x04);	// Lock comms interface, enable high pass filter
-    ade7953Write(psADE7953, ade7953REG_UNLOCK, 0xAD);	// Unlock reg 0x0120
-    ade7953Write(psADE7953, ade7953REG_OPTIMUM, 0x30);	// enable optimum settings
-
+int ade7953Config(i2c_di_t * psI2C) {
+	int iRV = ade7953ReConfig(psI2C);
 	DevIDflag |= 1 << devID_ADE7953;
 	IF_SYSTIMER_INIT(debugTIMING, stADE7953R, stMICROS, "ADE7953RD", 1500, 15000);
 	IF_SYSTIMER_INIT(debugTIMING, stADE7953W, stMICROS, "ADE7953WR", 1500, 15000);
-
-	return (ade7953ReadConfig(psADE7953) & 0x8000) ? erSUCCESS : erFAILURE;
+	ade7953InitIRQ(psI2C->DevIdx);
+	return iRV;
 }
 
-int ade7953ReConfig(void * pVoid) { return ade7953Config(pVoid); }
+int ade7953ReConfig(i2c_di_t * psI2C) {
+	ade7953_t * psADE7953 = &ade7953[psI2C->DevIdx];
+	psADE7953->psI2C->Test = 1;							// disable handling error we expect to get
+	int iRV = ade7953Write(psADE7953, regCONFIG, regCONFIG_SWRST);
+	psADE7953->psI2C->Test = 0;
+    do {
+		vTaskDelay(pdMS_TO_TICKS(10));
+		iRV = ade7953Read(psADE7953, regIRQSTATA, &psADE7953->oth.is_a);
+	} while (iRV < erSUCCESS || (psADE7953->oth.is_a.RESET == 0));
+
+    ade7953Write(psADE7953, regCONFIG, 0x04);			// Lock comms interface, enable high pass filter
+    ade7953Write(psADE7953, regUNLOCK, 0xAD);			// Unlock reg 0x0120
+    ade7953Write(psADE7953, regOPTIMUM, 0x30);			// enable optimum settings
+    ade7953LoadNVSConfig(psI2C->DevIdx, 0);				// load default calibration
+	return (ade7953ReadConfig(psADE7953) & 0x8000) ? erFAILURE : erSUCCESS;
+}
+
 
 // ###################################### general support ##########################################
 
-u16_t ade7953ReadConfig(ade7953_t * psADE7953) {
-	ade7953Read(psADE7953, ade7953REG_CONFIG, &psADE7953->sCONFIG.val);
-	return psADE7953->sCONFIG.val;
-}
 
 // ############################### device reporting functions ######################################
 
@@ -230,48 +246,39 @@ void ade7953ReportAdjust(void) { }
 
 void ade7953ReportData(void) { }
 
-void ade7953ReportStatus(report_t * psRprt, ade7953_t * psADE7953) {
+void ade7953ReportStatus(report_t * psR, ade7953_t * psADE7953) {
 const char caStat1[] = "AEHFx=%d VAREHFx=%d VAEHFx=%d AEOFx=%d VAREOFx=%d VAEOFA=%d AP_NOLOADA=%d";
 const char caStat2[] = "VAR_NOLOADx=%d VA_NOLOADx=%d APSIGNx=%d VARSIGNx=%d ZXTO_Ix=%d ZXIx=%d OIx=%d";
 const char caStat3[] = "ZXTO=%d ZXV=%d OV=%d WSMP=%d CYCEND=%d SAG=%d RESET=%d CRC=%d";
-	wprintfx(psRprt, "IRQA: EN=0x%06X STAT==0x%06X", psADE7953->sIRQENA.val, psADE7953->sIRQSTATA.val);
-	wprintfx(psRprt, caStat1, psADE7953->sIRQSTATA.AEHFA,
-								psADE7953->sIRQSTATA.VAREHFA,
-								psADE7953->sIRQSTATA.VAEHFA,
-								psADE7953->sIRQSTATA.AEOFA,
-								psADE7953->sIRQSTATA.VAREOFA,
-								psADE7953->sIRQSTATA.VAEOFA,
-								psADE7953->sIRQSTATA.AP_NOLOADA);
-	wprintfx(psRprt, caStat2, psADE7953->sIRQSTATA.VAR_NOLOADA,
-								psADE7953->sIRQSTATA.VA_NOLOADA,
-								psADE7953->sIRQSTATA.APSIGN_A,
-								psADE7953->sIRQSTATA.VARSIGN_A,
-								psADE7953->sIRQSTATA.ZXTO_IA,
-								psADE7953->sIRQSTATA.ZXIA,
-								psADE7953->sIRQSTATA.OIA);
-	wprintfx(psRprt, caStat3, psADE7953->sIRQSTATA.ZXTO,
-								psADE7953->sIRQSTATA.ZXV,
-								psADE7953->sIRQSTATA.OV,
-								psADE7953->sIRQSTATA.WSMP,
-								psADE7953->sIRQSTATA.CYCEND,
-								psADE7953->sIRQSTATA.SAG,
-								psADE7953->sIRQSTATA.RESET,
-								psADE7953->sIRQSTATA.CRC);
-	wprintfx(psRprt, "IRQB: EN=0x%06X STAT==0x%06X", psADE7953->sIRQENB.val, psADE7953->sIRQSTATB.val);
-	wprintfx(psRprt, caStat1, psADE7953->sIRQSTATB.AEHFB,
-								psADE7953->sIRQSTATB.VAREHFB,
-								psADE7953->sIRQSTATB.VAEHFB,
-								psADE7953->sIRQSTATB.AEOFB,
-								psADE7953->sIRQSTATB.VAREOFB,
-								psADE7953->sIRQSTATB.VAEOFB,
-								psADE7953->sIRQSTATB.AP_NOLOADB);
-	wprintfx(psRprt, caStat2, psADE7953->sIRQSTATB.VAR_NOLOADB,
-								psADE7953->sIRQSTATB.VA_NOLOADB,
-								psADE7953->sIRQSTATB.APSIGN_B,
-								psADE7953->sIRQSTATB.VARSIGN_B,
-								psADE7953->sIRQSTATB.ZXTO_IB,
-								psADE7953->sIRQSTATB.ZXIB,
-								psADE7953->sIRQSTATB.OIB);
+	// Decode CONFIG register
+	wprintfx(psR, "CL=%d ZXE=%d ZXI=%d CRC=%d SWRST=%d ZXLFP=%d ", psADE7953->oth.cfg.COMM_LOCK,
+		psADE7953->oth.cfg.ZX_EDGE, psADE7953->oth.cfg.ZX_I, psADE7953->oth.cfg.CRC_ENABLE,
+		psADE7953->oth.cfg.SWRST, psADE7953->oth.cfg.ZXLPF);
+	wprintfx(psR, "REVP_P=%d REVP_CF=%d PFM=%d HPF_E=%d IEB=%d IEA=%d", psADE7953->oth.cfg.REVP_PULSE,
+			psADE7953->oth.cfg.REVP_CF, psADE7953->oth.cfg.PFMODE, psADE7953->oth.cfg.HPFEN, psADE7953->oth.cfg.INTENB, psADE7953->oth.cfg.INTENA);
+
+	// Decode IRQA registers
+	wprintfx(psR, "IRQA: EN=0x%06X STAT==0x%06X", psADE7953->oth.ie_a.val, psADE7953->oth.is_a.val);
+	wprintfx(psR, caStat1, psADE7953->oth.is_a.AEHFA, psADE7953->oth.is_a.VAREHFA,
+		psADE7953->oth.is_a.VAEHFA, psADE7953->oth.is_a.AEOFA, psADE7953->oth.is_a.VAREOFA,
+		psADE7953->oth.is_a.VAEOFA, psADE7953->oth.is_a.AP_NOLOADA);
+	wprintfx(psR, caStat2, psADE7953->oth.is_a.VAR_NOLOADA, psADE7953->oth.is_a.VA_NOLOADA,
+		psADE7953->oth.is_a.APSIGN_A, psADE7953->oth.is_a.VARSIGN_A, psADE7953->oth.is_a.ZXTO_IA,
+		psADE7953->oth.is_a.ZXIA, psADE7953->oth.is_a.OIA);
+	wprintfx(psR, caStat3, psADE7953->oth.is_a.ZXTO, psADE7953->oth.is_a.ZXV, psADE7953->oth.is_a.OV,
+		psADE7953->oth.is_a.WSMP, psADE7953->oth.is_a.CYCEND, psADE7953->oth.is_a.SAG,
+		psADE7953->oth.is_a.RESET, psADE7953->oth.is_a.CRC);
+
+	#if (ade7953USE_CH2 > 0)
+	// Decode IRQB registers
+	wprintfx(psR, "IRQB: EN=0x%06X STAT==0x%06X", psADE7953->oth.ie_b.val, psADE7953->oth.is_b.val);
+	wprintfx(psR, caStat1, psADE7953->oth.is_b.AEHFB, psADE7953->oth.is_b.VAREHFB,
+		psADE7953->oth.is_b.VAEHFB, psADE7953->oth.is_b.AEOFB, psADE7953->oth.is_b.VAREOFB,
+		psADE7953->oth.is_b.VAEOFB, psADE7953->oth.is_b.AP_NOLOADB);
+	wprintfx(psR, caStat2, psADE7953->oth.is_b.VAR_NOLOADB, psADE7953->oth.is_b.VA_NOLOADB,
+		psADE7953->oth.is_b.APSIGN_B,psADE7953->oth.is_b.VARSIGN_B, psADE7953->oth.is_b.ZXTO_IB,
+		psADE7953->oth.is_b.ZXIB, psADE7953->oth.is_b.OIB);
+	#endif
 }
 
 void ade7953Report(void) { }
